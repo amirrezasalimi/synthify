96f2453b0a8e528e0ceba794795710bd3566a667
seville <amirrezasalimi0@gmail.com>
2024-04-07 20:57:36 +0330

    party

--- party/src/server.ts
+++ party/src/server.ts
@@ -1,31 +1,12 @@
 import type * as Party from "partykit/server";
+import { onConnect } from "y-partykit";
 
-export default class Server implements Party.Server {
-  constructor(readonly room: Party.Room) {}
 
-  onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-    // A websocket just connected!
-    console.log(
-      `Connected:
-  id: ${conn.id}
-  room: ${this.room.id}
-  url: ${new URL(ctx.request.url).pathname}`
-    );
-
-    // let's send a message to the connection
-    conn.send("hello from server");
-  }
-
-  onMessage(message: string, sender: Party.Connection) {
-    // let's log the message
-    console.log(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
-    this.room.broadcast(
-      `${sender.id}: ${message}`,
-      // ...except for the connection it came from
-      [sender.id]
-    );
+export default class YjsServer implements Party.Server {
+  constructor(public party: Party.Room) {}
+  onConnect(conn: Party.Connection) {
+    return onConnect(conn, this.party, {
+      // ...options
+    });
   }
 }
-
-Server satisfies Party.Worker;
