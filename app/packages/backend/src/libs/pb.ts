30a92cb893af6e202aa9e7afb8334d27b4ca8f8b
seville <amirrezasalimi0@gmail.com>
2024-04-16 14:00:09 +0330

    Fix bug in pb.ts file

--- app/packages/backend/src/libs/pb.ts
+++ app/packages/backend/src/libs/pb.ts
@@ -1,18 +1,6 @@
 import { TypedPocketBase } from "../types/pocketbase";
 import Pocketbase from "pocketbase";
 const pocketbase = async () => {
-  console.log(
-    "Connecting to Pocketbase...",
-    process.env.POCKETBASE_HOST,
-    process.env.POCKETBASE_EMAIL,
-    process.env.POCKETBASE_PASSWORD
-  );
-
-  // fetch host and log result
-  await fetch(process.env.POCKETBASE_HOST ?? "")
-    .then((response) => response.json())
-    .then((data) => console.log(`Host: ${data.host}`, data))
-    .catch((error) => console.error("Error fetching host:", error));
 
   const _ = new Pocketbase(process.env.POCKETBASE_HOST) as TypedPocketBase;
 
