d0a500f2b92108cb926e7748af50683ca7a341ba
seville <amirrezasalimi0@gmail.com>
2024-05-03 06:14:17 +0330

    chore: ai service validation

--- app/packages/frontend/src/modules/project/hooks/run.ts
+++ app/packages/frontend/src/modules/project/hooks/run.ts
@@ -3,6 +3,7 @@ import useFlows from "./flows";
 import toast from "react-hot-toast";
 import useProject from "./project";
 import { useReactFlow } from "reactflow";
+import { FlowBlock } from "../types/flow-data";
 
 const useRun = () => {
   const flows = useFlows();
@@ -10,12 +11,32 @@ const useRun = () => {
   const project = useProject();
 
   const rf = useReactFlow();
+  const aiServices = trpc.project.list_ai_services.useQuery();
+
+  const isAiServiceExists = (block: FlowBlock) => {
+    if (!block?.ai_config?.service) {
+      return false;
+    }
+    const service = aiServices.data?.find(
+      (service) => service.id == block.ai_config.service
+    );
+    return service;
+  };
   const softValidation = () => {
     for (const flow of flows.nodes) {
       for (const block of flow.data.blocks) {
         if (block.type == "llm" || block.type == "list") {
           if (!block.ai_config.service) {
             toast.error("Please select AI service for all LLM/List blocks");
+            rf.fitView({
+              nodes: [{ id: flow.id }],
+              duration: 500,
+              maxZoom: 1,
+            });
+            return false;
+          }
+          if (!isAiServiceExists(block)) {
+            toast.error("AI service not found");
             rf.fitView({
               nodes: [{ id: flow.id }],
               duration: 500,
