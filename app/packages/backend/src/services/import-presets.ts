04ecd19aefe92232ccb6121f73687e123f811fde
Amirreza <amirrezasalimi0@gmail.com>
2024-05-17 12:47:34 +0330

    chore: handle presets errors

--- app/packages/backend/src/services/import-presets.ts
+++ app/packages/backend/src/services/import-presets.ts
@@ -1,22 +1,25 @@
 import { pb } from "@/libs/pb";
 import presets from "@/data/presets.json";
 const importPresets = async () => {
-  try{
-    presets.forEach(async (preset) => {
-      const oldPresets = await pb.collection("presets").getFullList();
+  try {
+    const oldPresets = await pb.collection("presets").getFullList();
+
+    for (const preset of presets) {
       const exists = oldPresets.find(
         (oldPreset) =>
           oldPreset.title === preset.title &&
           oldPreset.category === preset.category
       );
+
       if (!exists) {
         await pb.collection("presets").create(preset);
         console.log(`Preset Imported : ${preset.title}`);
-      } 
-    });
+      }
+    }
   } catch (e) {
     console.error(e);
   }
 };
+
 
 export default importPresets;
