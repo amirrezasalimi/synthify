8d55d3a230a95e4b39ef80471e83bf9cbd7b70d7
seville <amirrezasalimi0@gmail.com>
2024-05-18 03:29:21 +0330

    chore: simplify pb auth

--- app/packages/backend/src/libs/pb.ts
+++ app/packages/backend/src/libs/pb.ts
@@ -1,41 +1,24 @@
 import { TypedPocketBase } from "../types/pocketbase";
 import Pocketbase from "pocketbase";
 
-let authToken: string | null = null;
 const initPb = async (instance: TypedPocketBase) => {
   console.log("Connecting to Pocketbase...", process.env.POCKETBASE_HOST);
+
   try {
-    if (authToken) {
-      await instance.authStore.save(authToken);
-      try {
-        // get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
-        instance.authStore.isValid && await instance.admins.authRefresh();
-      } catch (e: any) {
-        // clear the auth store on failed refresh
-        instance.authStore.clear();
-        authToken = "";
-        console.log("if (authToken) originalError",e.originalError)
-      }
-      return instance;
-    }
     const authData = await instance.admins
       .authWithPassword(
         process.env.POCKETBASE_EMAIL || "",
         process.env.POCKETBASE_PASSWORD || ""
       );
-    authToken = authData.token;
-    try {
-      // get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
-      instance.authStore.isValid && await instance.admins.authRefresh();
-    } catch (e: any) {
-      // clear the auth store on failed refresh
-      instance.authStore.clear();
-      console.log("authRefresh originalError",e.originalError)
-
+    if (authData.token) {
+      console.log("Connected to Pocketbase");
+    } else {
+      console.log("Failed to connect to Pocketbase");
     }
-  } catch (e) {
-    console.log("originalError",e.originalError)
-    console.log(e?.message);
+  } catch (e: any) {
+    console.log("Failed to connect to Pocketbase");
+    instance.authStore.clear();
+    console.log("authRefresh originalError", e.originalError)
   }
   return instance;
 };
@@ -45,10 +28,9 @@ const pbInstance = () => {
   const _ = new Pocketbase(process.env.POCKETBASE_HOST) as TypedPocketBase;
   _.autoCancellation(false);
   _.beforeSend = function (url, options) {
-    console.log("PB: ",url,options);
-    
+    console.log("- pb: ", url);
     return { url, options };
-};
+  };
   return _;
 };
 const pb = pbInstance();
