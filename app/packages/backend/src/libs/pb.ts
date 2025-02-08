7bb3380b602dfc9fcd0bd089fb495f2d2cf9a7dd
seville <amirrezasalimi0@gmail.com>
2025-02-08 22:12:41 +0330

    fix docker

--- app/packages/backend/src/libs/pb.ts
+++ app/packages/backend/src/libs/pb.ts
@@ -17,8 +17,8 @@ const pbInstance = (token?: string) => {
       );
 
       options.headers = {
-        Authorization: token,
         ...options.headers,
+        Authorization: token,
       };
       return {
         url,
